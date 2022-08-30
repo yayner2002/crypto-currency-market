474f3fddaf704e49fe35e6bc8a31055537522ac1
yayner <yayner2002@gmail.com>
2022-08-30 22:39:37 +0300

    Add header routing

--- src/components/pages/CoinDetail.js
+++ src/components/pages/CoinDetail.js
@@ -1,69 +1,69 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
-import Header from './Header';
-import ShowCase from './ShowCase';
+import ShowCase from '../Showcase/ShowCase';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { selectCoins } from '../../redux/crypto/Cryptos';
 import '../../styles/CryptoDetail.css';
+import Helmet from '../Helmet/Helmet';
 
 const CoinDetail = () => {
   const { id } = useParams();
   const coins = useSelector(selectCoins);
   const coinDetail = coins.filter((coin) => coin.id === id);
-  window.console.log(coinDetail);
 
   return (
 
-    <>
-      <Header />
-      <ShowCase />
-      <h3>Coin Detail</h3>
-      <div className="singleCoinDetail">
-        {coinDetail.map((coin) => (
-          <>
-            <div key={coin.name} className="d-flex justify-content-between">
-              <p>Name:</p>
-              <p>{coin.name}</p>
-            </div>
-            <div key={coin.name} className="d-flex justify-content-between">
-              <p>Symbol:</p>
-              <p>{coin.symbol}</p>
-            </div>
-            <div key={coin.name} className="d-flex justify-content-between">
-              <p>Rank:</p>
-              <p>{coin.rank}</p>
-            </div>
-            <div key={coin.name} className="d-flex justify-content-between">
-              <p>Supply:</p>
-              <p>{coin.supply}</p>
-            </div>
-            <div key={coin.name} className="d-flex justify-content-between">
-              <p>Market Capital(USD):</p>
-              <p>{coin.marketCapUsd}</p>
-            </div>
-            <div key={coin.name} className="d-flex justify-content-between">
-              <p>Volume(USD) 24Hrs:</p>
-              <p>{coin.volumeUsd24Hr}</p>
-            </div>
-            <div key={coin.name} className="d-flex justify-content-between">
-              <p>Price(USD):</p>
-              <p>{coin.priceUsd}</p>
-            </div>
-            <div key={coin.name} className="d-flex justify-content-between">
-              <p>Change Percent(24Hr):</p>
-              <p>{coin.changePercent24Hr}</p>
-            </div>
-            <div key={coin.name} className="d-flex justify-content-between">
-              <p>VWAP(24Hr):</p>
-              <p>{coin.vwap24Hr}</p>
-            </div>
+    <Helmet title="CoinDetail">
+      <>
+        <ShowCase />
+        <h5>Coin Detail</h5>
+        <ul className="singleCoinDetail">
+          {coinDetail.map((coin) => (
+            <>
+              <li key={coin.name} className="d-flex justify-content-between p-2">
+                <p>Name:</p>
+                <p>{coin.name}</p>
+              </li>
+              <li key={coin.name} className="d-flex justify-content-between p-2">
+                <p>Symbol:</p>
+                <p>{coin.symbol}</p>
+              </li>
+              <li key={coin.name} className="d-flex justify-content-between p-2">
+                <p>Rank:</p>
+                <p>{coin.rank}</p>
+              </li>
+              <li key={coin.name} className="d-flex justify-content-between p-2">
+                <p>Supply:</p>
+                <p>{coin.supply}</p>
+              </li>
+              <li key={coin.name} className="d-flex justify-content-between p-2">
+                <p>Market Capital(USD):</p>
+                <p>{coin.marketCapUsd}</p>
+              </li>
+              <li key={coin.name} className="d-flex justify-content-between p-2">
+                <p>Volume(USD) 24Hrs:</p>
+                <p>{coin.volumeUsd24Hr}</p>
+              </li>
+              <li key={coin.name} className="d-flex justify-content-between p-2">
+                <p>Price(USD):</p>
+                <p>{coin.priceUsd}</p>
+              </li>
+              <li key={coin.name} className="d-flex justify-content-between p-2">
+                <p>Change Percent(24Hr):</p>
+                <p>{coin.changePercent24Hr}</p>
+              </li>
+              <li key={coin.name} className="d-flex justify-content-between p-2">
+                <p>VWAP(24Hr):</p>
+                <p>{coin.vwap24Hr}</p>
+              </li>
 
-          </>
-        ))}
-      </div>
+            </>
+          ))}
+        </ul>
 
-    </>
+      </>
+    </Helmet>
 
   );
 };
