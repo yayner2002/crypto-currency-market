b5b65309c35fc89834fe9f7db249dbb03de7205c
yayner <yayner2002@gmail.com>
2022-09-03 01:37:00 +0300

    Solve the Bug in the Eslint and update snapshot test

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,6 +1,3 @@
-/* eslint-disable array-callback-return */
-/* eslint-disable consistent-return */
-/* eslint-disable jsx-a11y/control-has-associated-label */
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
